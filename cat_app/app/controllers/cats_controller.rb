8e97eaccc29da6be608110d6b064bc69fea100fb
Heidi Powers <heidipowers@gmail.com>
2016-07-30 12:07:47 -0400

    Getting cat info from both API's

--- cat_app/app/controllers/cats_controller.rb
+++ cat_app/app/controllers/cats_controller.rb
@@ -1,2 +1,18 @@
 class CatsController < ApplicationController
+  def index
+    getCats = {}
+    getCats[:quote] = HTTParty.get("http://catfacts-api.appspot.com/api/facts?number=1")
+    getCats[:pic] = HTTParty.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=1")
+
+    render json: getCats
+
+    # url = "http://catfacts-api.appspot.com/api/facts?number= "
+    # response = HTTParty.get(url)
+    # parsed_body = JSON.parse(response.body)
+    # render json: parsed_body
+    #url = "http://thecatapi.com/api/images/get?format=xml&results_per_page=20"
+    #response = HTTParty.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=5")
+    #render json: response
+  end
 end
+
